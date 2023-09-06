727403f2939ebdaafa362b2e029208c1ac112e4b
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-09-06 13:20:40 -0600

    a dded java fixed bug created another bug

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,38 @@
 //Sortable Function through jQueryUI - Fillip
-$( function() {
-    $( "#sortable1, #sortable2, #sortable3, #sortable4, #sortable5" ).sortable({
+$(function () {
+  $("#sortable1, #sortable2, #sortable3, #sortable4, #sortable5")
+    .sortable({
       connectWith: ".connectedSortable",
-      containment: ".quizsection"
-    }).disableSelection();
-  } );
+      containment: ".quizsection",
+    })
+    .disableSelection();
+});
 
+// weather js
+
+var destination = $("#locationName");
+
+$(document).ready(function () {
+  $("#dropDown").on("change", getWeather);
+
+  function getWeather() {
+    var selectedOption = $("#dropDown").val();
+    var requestWeather =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      selectedOption +
+      "&APPID=6125957e3b746825efbf44ae31af7452";
+
+    $.ajax({
+      url: requestWeather,
+      method: "GET",
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        $("#locationName").text(selectedOption);
+      },
+      error: function (error) {
+        console.error(error);
+      },
+    });
+  }
+});
