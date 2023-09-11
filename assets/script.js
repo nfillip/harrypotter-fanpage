115ba11c3724ed64dbed290da25b8b3c322439ac
nfillip <nick.m.fillip@gmail.com>
2023-09-11 10:47:45 -0600

    merging

--- assets/script.js
+++ assets/script.js
@@ -114,7 +114,7 @@ function testAnswers() {
       var liBorderEdit = randomizeSection.children().eq(x).children().eq(y);
       if (GSRH[x].indexOf(liTextItem) === -1) {
         $(liBorderEdit).css({
-          "background-color": "red",
+          "background-color": "rgba(255, 0, 0, 0.3",
           "color": "white",
         });
         testCorrect = false;
